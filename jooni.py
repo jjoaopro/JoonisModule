7c5ee68b528a2878af3d019d1aaea302bf191dcf
Jooniツ <50370102+jjoaopro@users.noreply.github.com>
2019-05-21 10:43:40 +0100

    Add files via upload

---
+++ jooni.py
@@ -0,0 +1,6 @@
+def greet(name):
+	print("Hello, " + name + ". Good morning!")
+
+
+def ungreet(name):
+	print("Hey, " + name + ", fook off!")
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='Jooni',
+      version='1.0',
+      description='Arithmetic Distribution Module',
+      author='Your Name',
+      author_email='Your Email',
+      url='The URL project - github for example',
+      py_modules=['jooni'],
+     )
